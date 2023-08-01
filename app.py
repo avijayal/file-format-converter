03ebba08b006de68fd3e1b90c8fb14225b4c6112
vijayalakshmi <3002.vijayalakshmi@gmail.com>
2023-08-01 11:56:21 +0530

    Refactor core conversion logic using process_file function

--- app.py
+++ app.py
@@ -9,7 +9,42 @@ import glob
 import pandas as pd
 
 
+# Avoiding hard coding using environment variables
+# def get_columns(ds):
+#     schemas_file_path = os.environ.setdefault('SCHEMAS_FILE_PATH','data/retail_db/schemas.json')
+#     with open(schemas_file_path)as fp:
+#         schemas=json.load(fp)
+#     try:
+#         file=schemas.get(ds)
+#         if not file:
+#             raise KeyError
+#         cols=sorted(file,key=lambda s:s['column_position'])
+#         columns=[col['column_name']for col in cols]
+#         return columns
+#     except KeyError:
+#         print(f'schema not found for {ds}')
+#         return
+# def main():
+#     src_base_dir = os.environ['SRC_BASE_DIR']
+#     tgt_base_dir = os.environ['TGT_BASE_DIR']   
+#     for path in glob.glob(f'{src_base_dir}/*'):
+#         if os.path.isdir(path):
+#             ds=os.path.split(path)[1]
+#             for file in glob.glob(f'{path}/*'):
+#                 df=pd.read_csv(file,names=get_columns(ds))
+#                 os.makedirs(f'{tgt_base_dir}/{ds}',exist_ok=True)
+#                 df.to_json(
+#                     f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',
+#                     orient='records',
+#                     lines=True
+#                 )
+#                 print(f'no.of records processed for {os.path.split(file)[1]} in {ds} is {df.shape[0]}')
 
+
+# if __name__=='__main__':
+#     main()
+    
+# modularizing using environment variables
 def get_columns(ds):
     schemas_file_path = os.environ.setdefault('SCHEMAS_FILE_PATH','data/retail_db/schemas.json')
     with open(schemas_file_path)as fp:
@@ -24,23 +59,26 @@ def get_columns(ds):
     except KeyError:
         print(f'schema not found for {ds}')
         return
+    
+def proces_file(src_base_dir,ds,tgt_base_dir):
+    for file in glob.glob(f'{src_base_dir}/{ds}/part*'):
+        df=pd.read_csv(file,names=get_columns(ds))
+        os.makedirs(f'{tgt_base_dir}/{ds}',exist_ok=True)
+        df.to_json(
+            f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',
+            orient='records',
+            lines=True
+        )
+        print(f'no.of records processed for {os.path.split(file)[1]} in {ds} is {df.shape[0]}')
+
 def main():
     src_base_dir = os.environ['SRC_BASE_DIR']
     tgt_base_dir = os.environ['TGT_BASE_DIR']   
     for path in glob.glob(f'{src_base_dir}/*'):
         if os.path.isdir(path):
-            ds=os.path.split(path)[1]
-            for file in glob.glob(f'{path}/*'):
-                df=pd.read_csv(file,names=get_columns(ds))
-                os.makedirs(f'{tgt_base_dir}/{ds}',exist_ok=True)
-                df.to_json(
-                    f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',
-                    orient='records',
-                    lines=True
-                )
-                print(f'no.of records processed for {os.path.split(file)[1]} in {ds} is {df.shape[0]}')
+            proces_file(src_base_dir,os.path.split(path)[1],tgt_base_dir)
+ 
 
 
 if __name__=='__main__':
     main()
-    
